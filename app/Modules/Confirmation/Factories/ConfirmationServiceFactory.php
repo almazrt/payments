a153c54ba53a8b128e186b0d31f80ba9f46f79f9
Almaz <almazrt@gmail.com>
2023-01-09 23:02:02 +0300

    Add factory for confirmation services

---
+++ app/Modules/Confirmation/Factories/ConfirmationServiceFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Modules\Confirmation\Factories;
+
+use App\Modules\Confirmation\Contracts\ConfirmationServiceInterface;
+use App\Modules\Confirmation\Data\ConfirmationMethod;
+use App\Modules\Confirmation\Services\EmailConfirmationService;
+use App\Modules\Confirmation\Services\SmsConfirmationService;
+use App\Modules\Confirmation\Services\TelegramConfirmationService;
+use Illuminate\Container\Container;
+
+class ConfirmationServiceFactory
+{
+    public function __construct(private readonly Container $container)
+    {
+    }
+
+    public function build(ConfirmationMethod $confirmationMethod): ConfirmationServiceInterface
+    {
+        return $this->container->make(match ($confirmationMethod) {
+            ConfirmationMethod::Email => EmailConfirmationService::class,
+            ConfirmationMethod::Sms => new SmsConfirmationService::class,
+            ConfirmationMethod::Telegram => new TelegramConfirmationService::class,
+        });
+    }
+}
