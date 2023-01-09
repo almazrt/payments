99d69f1741890ee2241b5042d8f91afe488cb76e
Almaz <almazrt@gmail.com>
2023-01-09 23:02:56 +0300

    Add user settings service

---
+++ app/Modules/UserSettings/Services/UserSettingsService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Modules\UserSettings\Services;
+
+use App\Modules\Confirmation\Exceptions\InvalidCodeException;
+use App\Modules\Confirmation\Factories\ConfirmationServiceFactory;
+use App\Modules\UserSettings\Data\UpdatingUserSettingDto;
+use App\Modules\UserSettings\Repositories\UserSettingsRepository;
+
+class UserSettingsService
+{
+    public function __construct(
+        private readonly UserSettingsRepository     $userSettingsRepository,
+        private readonly ConfirmationServiceFactory $confirmationServiceFactory,
+    )
+    {
+    }
+
+    /**
+     * @throws InvalidCodeException
+     */
+    public function setUserSetting(UpdatingUserSettingDto $updatingUserSettingDto): void
+    {
+        $confirmationService = $this->confirmationServiceFactory->build($updatingUserSettingDto->confirmationMethod);
+        if (!$confirmationService->checkCode($updatingUserSettingDto->confirmationId, $updatingUserSettingDto->code)) {
+            throw new InvalidCodeException;
+        }
+
+        $this->userSettingsRepository->updateUserSetting(
+            $updatingUserSettingDto->userId,
+            $updatingUserSettingDto->userSetting,
+            $updatingUserSettingDto->newValue,
+        );
+    }
+}
